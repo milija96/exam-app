f2246255aba38fdf8fc4cf884d3d22e3c67a0fa6
milija <nedovicmilija@gmail.com>
2019-05-07 16:03:04 +0200

    Local functionalities

--- src/app/layout/login/login.component.ts
+++ src/app/layout/login/login.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
 import { AuthService } from 'src/app/services/auth.service';
-=======
->>>>>>> c2794f9dcc36da8482b89e757b8ab20d3108a4f1
 
 @Component({
   selector: 'app-login',
@@ -11,19 +8,12 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class LoginComponent implements OnInit {
 
-<<<<<<< HEAD
   constructor(private auth: AuthService) { }
-=======
-  constructor() { }
->>>>>>> c2794f9dcc36da8482b89e757b8ab20d3108a4f1
 
   ngOnInit() {
   }
 
-<<<<<<< HEAD
   logIn(data: any) {
       this.auth.singIn(data.value);
   }
-=======
->>>>>>> c2794f9dcc36da8482b89e757b8ab20d3108a4f1
 }
--- src/app/layout/menu/top-menu/top-menu.component.ts
+++ src/app/layout/menu/top-menu/top-menu.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
 import { ISubjects } from 'src/app/models/models';
 import { Subject, ReplaySubject } from 'rxjs';
 import { SubjectService } from 'src/app/services/subject.service';
-=======
->>>>>>> c2794f9dcc36da8482b89e757b8ab20d3108a4f1
 
 @Component({
   selector: 'app-top-menu',
@@ -12,7 +9,6 @@ import { SubjectService } from 'src/app/services/subject.service';
   styleUrls: ['./top-menu.component.css']
 })
 export class TopMenuComponent implements OnInit {
-<<<<<<< HEAD
   availableSubjects: Array<ISubjects> = [
     {id: 1,  name: 'Softversko inzenjerstvo'},
     {id: 2,  name: 'Internet programiranje'}]
@@ -34,12 +30,6 @@ export class TopMenuComponent implements OnInit {
   subjectChange(sub: any) {
     this.currentSubject.next(sub);
     this.subject.currentSubject$.next(sub)
-=======
-
-  constructor() { }
-
-  ngOnInit() {
->>>>>>> c2794f9dcc36da8482b89e757b8ab20d3108a4f1
   }
 
 }
